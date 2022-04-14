59f2f8ec89b7a576dccd9e86dfad45b1483be864
Thiago Vieira <thiagosvieira89@gmail.com>
2022-04-13 23:28:42 -0300

    chore: Configuração inicial. DRF, DJANGO, BLACK, DRF_YASG

---
+++ backend/core/urls.py
@@ -0,0 +1,23 @@
+from django.contrib import admin
+from django.urls import path, include
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework import permissions
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Tibia Explorer API",
+        default_version="v1",
+        description="Swagger UI to the Tibia Explorer API",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="contact@tibia_explorer.com.br"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny],
+)
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("", schema_view.with_ui("swagger", cache_timeout=0), name="schema-swagger-ui"),
+]
